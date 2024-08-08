94804178b9aba984968d3693cdd867b269db43c1
Emediong Bassey <basseyemediong@gmail.com>
2024-08-08 11:36:04 +0100

    commit new changes

--- app/Filament/pages/Dashboard.php
+++ app/Filament/pages/Dashboard.php
@@ -10,12 +10,6 @@ class Dashboard extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.users.pages.dashboard';
-    
-    public static function shouldRegisterNavigation(): bool
-        {
-            if(auth()->user()->hasRole(['admin'])){
-                return false;
-            }
-            return  true;
-        }
+
+   
 }
